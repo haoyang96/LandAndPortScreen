d49f2af47d956d2f1a805b82a113904f45672e55
unknown <>
2016-09-04 16:08:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/landandportscreen/AnotherActivity.java
@@ -0,0 +1,14 @@
+package com.example.landandportscreen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 杨浩 on 2016/9/4.
+ */
+public class AnotherActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
